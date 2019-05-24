1e19c8b83df4db8002efcf7d74890e13e9bb03bf
danmohaha <liyuezun@gmail.com>
2019-05-24 14:40:24 -0400

    update

--- demo.py
+++ demo.py
@@ -30,8 +30,10 @@ pwd = os.path.dirname(__file__)
 front_face_detector = dlib.get_frontal_face_detector()
 lmark_predictor = dlib.shape_predictor(pwd + '/dlib_model/shape_predictor_68_face_landmarks.dat')
 
-# Save face and matrix to cache
-sess = tf.Session()
+tfconfig = tf.ConfigProto(allow_soft_placement=True)
+tfconfig.gpu_options.allow_growth=True
+# init session
+sess = tf.Session(config=tfconfig)
 # Build network
 reso_net = ResoNet(cfg=cfg, is_train=False)
 reso_net.build()
